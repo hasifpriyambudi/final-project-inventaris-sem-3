233fbbb6476e64efc2c18e31e602f8fb8b1fc626
hasifpriyambudi <hasifpriyambudi13gmail.com>
2021-12-17 03:24:19 +0700

    first commit

---
+++ inventtaris/view/Dash.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace inventtaris.view{
+    /// <summary>
+    /// Interaction logic for Dash.xaml
+    /// </summary>
+    public partial class Dash : Window{
+        public Dash(){
+            InitializeComponent();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FrameMain.Navigate(new view.pageHome());
+        }
+
+        private void menuMember_MouseDown(object sender, MouseButtonEventArgs e){
+            FrameMain.Navigate(new view.pageMember());
+        }
+
+        private void menuBarang_MouseDown(object sender, MouseButtonEventArgs e){
+            FrameMain.Navigate(new view.pageBarang());
+        }
+
+        private void menuPeminjaman_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FrameMain.Navigate(new view.pagePeminjaman());
+        }
+
+        private void menuPengembalian_MouseDown(object sender, MouseButtonEventArgs e){
+            FrameMain.Navigate(new view.pagePengembalian());
+        }
+
+        private void menuLogout_MouseDown(object sender, MouseButtonEventArgs e){
+            login login= new login();
+            login.Show();
+            this.Close();
+        }
+    }
+}
---
+++ inventtaris/view/login.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace inventtaris.view{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window{
+        public login(){
+            InitializeComponent();
+        }
+
+        private void btnLoginProses(object sender, RoutedEventArgs e){
+            Dash dash = new Dash();
+            dash.Show();
+            this.Close();
+        }
+    }
+}
