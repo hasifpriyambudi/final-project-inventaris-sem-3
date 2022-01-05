19bc5be448ff2450c25f4182a043aca4892f0819
Hasif Priyambudi <hasifpriyambudi@gmail.com>
2022-01-05 23:04:32 +0700

    fix tampilan admin

--- inventtaris/view/pageAdmin.xaml.cs
+++ inventtaris/view/pageAdmin.xaml.cs
@@ -11,16 +11,40 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace inventtaris.view
-{
+namespace inventtaris.view{
     /// <summary>
     /// Interaction logic for pageAdmin.xaml
     /// </summary>
-    public partial class pageAdmin : Page
-    {
-        public pageAdmin()
-        {
+    public partial class pageAdmin : Page{
+        public pageAdmin(){
             InitializeComponent();
+        }
+
+        private void btnTambah(object sender, RoutedEventArgs e){
+            
+        }
+
+        private void dgadmin_SelectionChanged(object sender, SelectionChangedEventArgs e){
+            
+        }
+
+        private void cancel(){
+            
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e){
+            
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e){
+            
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e){
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e){
+            
         }
     }
 }
